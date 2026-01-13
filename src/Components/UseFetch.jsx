16d244420956d3dbf497ddb13cb33b62bb73835e
g ve <gloria.laura.vela@gmail.com>
2026-01-12 19:10:03 -0500

    Create the useFetch component

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,17 @@
+import {useState, useEffect } from "react";
+
 const UseFetch = (url) => {
-    
+    const[data,setData]=useState();
+    // This is a React Hook used for performing side effects in functional components. It’s typically used for fetching data, subscribing to events, or any other side effects that don’t involve rendering.
+     useEffect(()=>{
+        //This initiates an HTTP request to the specified url.
+        fetch(url)
+        //This converts the response from the server to JSON format.
+        .then((res)=>res.json())
+        //This sets the retrieved data to the state variable data. setData is a function that updates the state in React functional components.
+        .then((data)=>setData(data))
+    },[])
+return [data]
 }
 
 export default UseFetch
